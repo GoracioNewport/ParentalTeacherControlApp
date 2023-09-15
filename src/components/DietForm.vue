4f93dbbd37ea3846fcf8a9694c235515b2ad08b0
Ivan Dolgikh <bdfyljkub@gmail.com>
2023-09-15 21:07:06 +0300

    fix: typo

--- src/components/DietForm.vue
+++ src/components/DietForm.vue
@@ -39,7 +39,7 @@
         
         <!-- Специалист -->
         <Listbox as="div" v-model="selectedTeacher" class="block mt-6">
-          <ListboxLabel class="block font-bold leading-6 text-gray-900">Спициалист:</ListboxLabel>
+          <ListboxLabel class="block font-bold leading-6 text-gray-900">Специалист:</ListboxLabel>
           <div class="relative mt-2">
             <ListboxButton class="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
               <span class="flex items-center">
